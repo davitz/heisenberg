109f7a8b51728c99e3e5448f1c72a0326fa46eee
dan <highfive.on.mars@gmail.com>
2016-05-06 15:11:05 -0400

    basic sprite movement, only sort of jumpy

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float Speed = 7.0f;
+
+    void Start() {}
+
+    void Update()
+    {
+
+        // Jump
+        float vertical = Input.GetAxis("Vertical");
+        if (vertical <= 0) vertical = 0.0f; 
+
+        // Apply movement vector with speed to current position
+        transform.position += new Vector3(Input.GetAxis("Horizontal"),vertical,0) * Speed * Time.deltaTime;
+    }
+}
