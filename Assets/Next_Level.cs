8811f5790c8205863ddeb0a0bdc2b4c768ba2d34
Niktib <nikolas.otto.maier@gmail.com>
2016-05-07 13:43:23 -0400

    Default Level and Portal Animation Switch
    Added the code so that when we have the animation, the portal can be
    flipped very simply.
    
    Also added a default level scene that works well on the Labtop but is
    easy to alter for any computer by changing the wall dimensions.

--- Assets/Next_Level.cs
+++ Assets/Next_Level.cs
@@ -5,16 +5,21 @@ using UnityStandardAssets._2D;
 public class Next_Level : MonoBehaviour {
     //Portal stuff, for finding out if the player has a key and sending them to the next level
     private PlatformerCharacter2D Player;
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private GameObject MyGO;
+    private bool KeyIsFound;
+
+    // Use this for initialization
+    void Start ()
+    {
+        MyGO = GameObject.FindWithTag("Player"); //We can get a reference to the player going now for the animation
+        Player = MyGO.GetComponent<PlatformerCharacter2D>();
+    }
+    //The portal teleport to new level check
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player") //Checks if the player is hitting the portal
         {
-            Player = other.GetComponent<PlatformerCharacter2D>();
-            if (Player.HasKey()) //Checks if the player has a key
+            if (KeyIsFound) //Checks if the player has a key
             {
                 //Put the effect to go to the next level here
                 Debug.Log("You won, go home");
@@ -22,7 +27,12 @@ public class Next_Level : MonoBehaviour {
         }
     }
 // Update is called once per frame
-void Update () {
-	
+    void Update ()
+    {
+        if (Player.HasKey() && !KeyIsFound) //Will trigger once, and will flip the animation for the portal from locked to open when it is ready
+        {
+            Debug.Log("Key is found");
+            KeyIsFound = true;
+        }
 	}
 }
