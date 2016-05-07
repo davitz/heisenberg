6b17d957b81617728269de50794951bf09a9894b
Niktib <nikolas.otto.maier@gmail.com>
2016-05-06 22:09:06 -0400

    Issue 23
    The spikes shall murder

---
+++ Assets/Respawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class Respawn : MonoBehaviour {
+    bool respawn;
+    Transform SpawnPoint;
+
+    // Use this for initialization
+    void Start() {
+        respawn = false;        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (respawn) { transform.position = SpawnPoint.position; }
+    }
+
+    void OnTriggerEnter(Collider2D other)
+    {
+        if (other.tag == "Player") { respawn = true; }
+    }
+}
