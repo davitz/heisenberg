d9cd43fd0e7373d3de4d0dddcc3c3a2eeacf3187
JessBradshaw <info@jessbradshaw.ca>
2016-05-07 08:50:50 -0400

    Adding EnableObjects script to help with tutorial
    created a script that enables objects if you set enable to true.
    Created prefab with triggerbox/text so that if the character hits the
    trigger text pops up.

---
+++ Assets/Scripts/EnableObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableObject : MonoBehaviour {
+	public bool enabled = false;
+	public GameObject enabledObject; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    //This is used to find if the player has touched the trigger and to enable selected game object. 
+    //spawn something or create popup.
+    //might need to add in a disable version (that is why I created enabled so we have the flexiblity to add it in) 
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Player"){
+			if(enabled)
+			{
+				enabledObject.SetActive(true);
+			}
+			else
+			{
+				enabledObject.SetActive(false);
+			}
+		}
+	} 
+}
