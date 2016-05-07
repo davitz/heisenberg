b26676f6a9d04812f532666ad8def9875b4b83ab
Davide De Simone <ddesimone1@gmail.com>
2016-05-07 00:11:31 -0400

    resolved conflicts

--- Assets/Standard Assets/2D/Scripts/Restarter.cs
+++ Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,22 +15,3 @@ namespace UnityStandardAssets._2D
         }
     }
 }
-=======
-using System;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-namespace UnityStandardAssets._2D
-{
-    public class Restarter : MonoBehaviour
-    {
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            if (other.tag == "Player")
-            {
-                Application.LoadLevel(Application.loadedLevel);
-            }
-        }
-    }
-}
->>>>>>> fb3e6362524c416674a5e281487b2baa3acba67c
