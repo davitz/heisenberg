25b1dd1da98f86e29d58d9a7cb2b32784151ccd9
Davide De Simone <ddesimone1@gmail.com>
2016-05-19 00:35:29 -0400

    added some more annotations, set up level traversal, fixed waveform effect on player, caused player to be assigned a random velocity if entering waveform from rest, added credits scene, adjusted build settings

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ExitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            Application.Quit();
+        }
+
+    }
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Submit"))
+        {
+            Application.LoadLevel(1);
+        }
+
+    }
+}
