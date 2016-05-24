70fca5ec1184174781d84d7e9aad4314b76726d7
Davide De Simone <ddesimone1@gmail.com>
2016-05-23 22:32:42 -0400

    allowed player to exit any time with Back or Esc

--- Assets/Scripts/Escape.cs
+++ Assets/Scripts/Escape.cs
@@ -2,15 +2,16 @@
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class ExitGame : MonoBehaviour {
+public class Escape : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (CrossPlatformInputManager.GetButtonDown("Cancel"))
         {
